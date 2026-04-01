4ff285c8bb7b612a62e7079f00a88dccc21361dd
Gilles Habran <gilleshabran@protonmail.com>
2026-04-01 21:27:36 +0200

    replace postgres k3s db by embedded etcd for k3s

--- roles/etcd/filter_plugins/etcd_filters.py
+++
@@ -1,9 +0,0 @@
-def format_etcd_peer(pair, port):
-    ip, name = pair
-    return f"{name}=http://{ip}:{port}"
-
-class FilterModule(object):
-    def filters(self):
-        return {
-            'format_etcd_peer': format_etcd_peer
-        }
